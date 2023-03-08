9d9b3bc28880f2b80838639ba43643a5c1fd3e4d
Ranecodes <ranehobasi@gmail.com>
2023-03-08 01:16:34 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <RepositoryGithub/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import RepositoryGithub from './components/Repository.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, RepositoryGithub
   }
 }
 </script>
---
+++ src/components/Repository.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <h1>Welcome to Github</h1>
+
+        <div class="github-repos" v-for="repo in repos" :key="repo.id">
+            <h2>{{ repo.name }}</h2>
+            <p>{{ repo.description }}</p>
+            <a :href="repo.html_url" target="_blank">View on Github</a>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+
+
+export default {
+    name: 'RepositoryGithub',
+    data() {
+        return {
+            repos: null,
+        }
+    },
+    created() {
+        axios.get('https://api.github.com/users/Ranecodes/repos')
+            .then(response => {
+                this.repos = response.data;
+            })
+    }
+}
+</script>
+
+<style>
+</style>

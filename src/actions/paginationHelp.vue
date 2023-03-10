50c56dd4e68884b6797b492b8582b2af5b11e59a
Ranecodes <ranehobasi@gmail.com>
2023-03-10 18:41:44 +0100

    pagination

--- src/actions/paginationHelp.vue
+++ src/actions/paginationHelp.vue
@@ -1,123 +1,83 @@
 <template>
-<!-- Pagination Component -->
-    <ul class="paginationContainer">
-        <li class="pagination-item">
-            <a href="#" @click.prevent="onClickFirstPage" :class="isInFirstPage? 'disabled':''" :disabled="isInFirstPage">First</a>
-        </li>
-        <li :key="page.id" v-for="page in pages" class="pagination-item">
-            <a href="#" @click.prevent="onClickPage(page.name)" :disabled="page.isDisabled" :class="{ active: isPageActive(page.name) }">{{ page.name }}</a>
-        </li>
-        <li class="pagination-item">
-            <a href="#" @click.prevent="onClickLastPage" :class="isInLastPage? 'disabled':''" :disabled="isInLastPage">Last</a>
-        </li>
-    </ul>
+    <div>
+        <slot>
+        </slot>
+        <div class="pagination-row">
+            <button class="pagination-btn" @click="prevPage">Prev</button>
+            <span v-for="(item, index) in new Array(7)" :key="index">
+                <button class="pagination-btn" >{{ index + 1 }}</button>
+            </span>
+            <button class="pagination-btn" @click="nextPage">Next</button>
+        </div>
+    </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'PaginationHelp',
-    props: {
-        total: {
-            type: Number,
-            required: true
-        },
-        perPage: {
-            type: Number,
-            required: true
-        },
-        currentPage: {
-            type: Number,
-            required: true
+    name: 'PaginationNum',
+    data() {
+        return {
+            repos: null,
+            recordsPerPage:  axios.get('https://api.github.com/users/Ranecodes/repos?page=1&per_page=2')
+            .then(response => {
+                this.repos = response.data;
+            }),
+            page: 1,
+        
         }
     },
-    computed: {
-        pages() {
-            let pages = [];
-            let totalPages = Math.ceil(this.total / this.perPage);
-            let startPage = 1;
-            let endPage = totalPages;
-            let isMaxSized = false;
-
-            if (totalPages > 7) {
-                isMaxSized = true;
-                startPage = Math.max(this.currentPage - 3, 1);
-                endPage = Math.min(startPage + 6, totalPages);
-            }
-
-            for (let i = startPage; i <= endPage; i++) {
-                pages.push({
-                    name: i,
-                    isDisabled: false
-                });
-            }
-
-            if (isMaxSized && startPage > 1) {
-                pages.unshift({
-                    name: '...',
-                    isDisabled: true
-                });
-                pages.unshift({
-                    name: 1,
-                    isDisabled: false
-                });
-            }
-
-            if (isMaxSized && endPage < totalPages) {
-                pages.push({
-                    name: '...',
-                    isDisabled: true
-                });
-                pages.push({
-                    name: totalPages,
-                    isDisabled: false
-                });
-            }
-
-            return pages;
-        },
-        isInFirstPage() {
-            return this.currentPage === 1;
-        },
-        isInLastPage() {
-            return this.currentPage === Math.ceil(this.total / this.perPage);
-        }
+    created() {
+        axios.get('https://api.github.com/users/Ranecodes/repos')
+            .then(response => {
+                this.repos = response.data;
+            }),
+            axios.get('https://api.github.com/users/Ranecodes/repos?page=1&per_page=2')
+            .then(response => {
+                this.repos = response.data;
+            })
     },
+
     methods: {
-        onClickPage(page) {
-            if (page === '...') {
-                return;
+            nextPage() {
+                this.page++;
+                axios.get('https://api.github.com/users/Ranecodes/repos?page=' + this.page + '&per_page=2')
+                .then(response => {
+                    this.repos = response.data;
+                })
+            },
+            prevPage() {
+                this.page--;
+                axios.get('https://api.github.com/users/Ranecodes/repos?page=' + this.page + '&per_page=2')
+                .then(response => {
+                    this.repos = response.data;
+                })
+            },
+            goToPage(page) {
+                this.page = page;
+                axios.get('https://api.github.com/users/Ranecodes/repos?page=' + this.page + '&per_page=2')
+                .then(response => {
+                    this.repos = response.data;
+                })
             }
-            this.$emit('page-changed', page);
-        },
-        onClickFirstPage() {
-            if (this.isInFirstPage) {
-                return;
-            }
-            this.$emit('page-changed', 1);
-        },
-        onClickLastPage() {
-            if (this.isInLastPage) {
-                return;
-            }
-            this.$emit('page-changed', Math.ceil(this.total / this.perPage));
-        },
-        isPageActive(page) {
-            return this.currentPage === page;
         }
     }
-}
 </script>
 
 <style>
-.paginationContainer {
+.pagination-row {
     display: flex;
     justify-content: center;
-    align-items: center;
-    list-style: none;
-    padding: 0;
-    margin: 0;
+    margin-top: 20px;
 }
-.pagination-item {
-    margin: 0 5px;
+
+.pagination-btn{
+    background-color: #f5f5f5;
+    padding: 15px;
+    margin: 7px;
+    border-radius: 5px;
+    border: none;
+    cursor: pointer;
 }
 </style>
